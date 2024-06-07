0445fc9728a8bf0f2cd3133f70c1b393f987b6ce
Ugase <106616134+Ugase@users.noreply.github.com>
2024-06-07 08:27:25 +0300

    one line

--- ai.py
+++ ai.py
@@ -1,13 +1,1 @@
-def calc(num1: float, num2: float, operator: str):
-    return eval(f"{num1} {operator} {num2}")
-
-def usr_input():
-    num = input("Number: ")
-    num2 = input("Number: ")
-    operator = input("Operator: ").strip(" ")
-    return [num, num2, operator]
-
-def main():
-    while True:
-        usr = usr_input()
-        print(calc(usr[0], usr[1], usr[2]))
+while True: print(eval(f"{input("Number: ")} {input("Operator: ").strip(" ")} {input("Number: ")}"))
